19364b481e8bb75631a47cfdf54f0bd9798c2d06
Yurino Oga <yurino.o@latona.io>
2021-12-19 21:36:36 +0900

    release

---
+++ SAP_API_Caller/caller.go
@@ -0,0 +1,177 @@
+package sap_api_caller
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	sap_api_output_formatter "sap-api-integrations-planned-order-reads/SAP_API_Output_Formatter"
+	"strings"
+	"sync"
+
+	"github.com/latonaio/golang-logging-library/logger"
+	"golang.org/x/xerrors"
+)
+
+type SAPAPICaller struct {
+	baseURL string
+	apiKey  string
+	log     *logger.Logger
+}
+
+func NewSAPAPICaller(baseUrl string, l *logger.Logger) *SAPAPICaller {
+	return &SAPAPICaller{
+		baseURL: baseUrl,
+		apiKey:  GetApiKey(),
+		log:     l,
+	}
+}
+
+func (c *SAPAPICaller) AsyncGetPlannedOrder(plannedOrder, material, mRPPlant, plant string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "Header":
+			func() {
+				c.Header(plannedOrder)
+				wg.Done()
+			}()
+		case "HeaderMaterialPlant":
+			func() {
+				c.HeaderMaterialPlant(material, mRPPlant)
+				wg.Done()
+			}()
+		case "ComponentMaterialPlant":
+			func() {
+				c.ComponentMaterialPlant(material, plant)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+
+func (c *SAPAPICaller) Header(plannedOrder string) {
+	data, err := c.callPlannedOrderSrvAPIRequirementHeader("PlannedOrderHeader", plannedOrder)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+}
+
+func (c *SAPAPICaller) callPlannedOrderSrvAPIRequirementHeader(api, plannedOrder string) ([]sap_api_output_formatter.Header, error) {
+	url := strings.Join([]string{c.baseURL, "api_plannedorder/srvd_a2x/sap/plannedorder/0001", api}, "/")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	c.setHeaderAPIKeyAccept(req)
+    c.getQueryWithHeader(req, plannedOrder)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, xerrors.Errorf("API status code %d. API request failed", resp.StatusCode)
+	}
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToHeader(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) HeaderMaterialPlant(material, mRPPlant string) {
+	data, err := c.callPlannedOrderSrvAPIRequirementHeaderMaterialPlant("PlannedOrderHeader", material, mRPPlant)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+}
+
+func (c *SAPAPICaller) callPlannedOrderSrvAPIRequirementHeaderMaterialPlant(api, material, mRPPlant string) ([]sap_api_output_formatter.Header, error) {
+	url := strings.Join([]string{c.baseURL, "api_plannedorder/srvd_a2x/sap/plannedorder/0001", api}, "/")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	c.setHeaderAPIKeyAccept(req)
+    c.getQueryWithHeaderMaterialPlant(req, material, mRPPlant)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, xerrors.Errorf("API status code %d. API request failed", resp.StatusCode)
+	}
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToHeader(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) ComponentMaterialPlant(material, plant string) {
+	data, err := c.callPlannedOrderSrvAPIRequirementComponentMaterialPlant("PlannedOrderComponent", material, plant)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+}
+
+func (c *SAPAPICaller) callPlannedOrderSrvAPIRequirementComponentMaterialPlant(api, material, plant string) ([]sap_api_output_formatter.Component, error) {
+	url := strings.Join([]string{c.baseURL, "api_plannedorder/srvd_a2x/sap/plannedorder/0001", api}, "/")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	c.setHeaderAPIKeyAccept(req)
+    c.getQueryWithComponentMaterialPlant(req, material, plant)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, xerrors.Errorf("API status code %d. API request failed", resp.StatusCode)
+	}
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToComponent(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) setHeaderAPIKeyAccept(req *http.Request) {
+	req.Header.Set("APIKey", c.apiKey)
+	req.Header.Set("Accept", "application/json")
+}
+
+func (c *SAPAPICaller) getQueryWithHeader(req *http.Request, plannedOrder string) {
+	params := req.URL.Query()
+	params.Add("$filter", fmt.Sprintf("PlannedOrder eq '%s'", plannedOrder))
+	req.URL.RawQuery = params.Encode()
+}
+
+func (c *SAPAPICaller) getQueryWithHeaderMaterialPlant(req *http.Request, material, mRPPlant string) {
+	params := req.URL.Query()
+	params.Add("$filter", fmt.Sprintf("Material eq '%s' and MRPPlant eq '%s'", material, mRPPlant))
+	req.URL.RawQuery = params.Encode()
+}
+
+func (c *SAPAPICaller) getQueryWithComponentMaterialPlant(req *http.Request, material, plant string) {
+	params := req.URL.Query()
+	params.Add("$filter", fmt.Sprintf("Material eq '%s' and Plant eq '%s'", material, plant))
+	req.URL.RawQuery = params.Encode()
+}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	sap_api_caller "sap-api-integrations-planned-order-reads/SAP_API_Caller"
+	"sap-api-integrations-planned-order-reads/sap_api_input_reader"
+
+	"github.com/latonaio/golang-logging-library/logger"
+)
+
+func main() {
+	l := logger.NewLogger()
+	fr := sap_api_input_reader.NewFileReader()
+	inoutSDC := fr.ReadSDC("./Inputs//SDC_Planned_Order_Component_Material_Plant_sample.json")
+	caller := sap_api_caller.NewSAPAPICaller(
+		"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata4/sap/", l, // Planned Order は URL の フォーマットが若干異なる
+	)
+
+	accepter := inoutSDC.Accepter
+	if len(accepter) == 0 || accepter[0] == "All" {
+		accepter = []string{
+			"Header", "HeaderMaterialPlant", "ComponentMaterialPlant",
+		}
+	}
+
+	caller.AsyncGetPlannedOrder(
+		inoutSDC.PlannedOrder.PlannedOrder,
+		inoutSDC.PlannedOrder.Material,
+		inoutSDC.PlannedOrder.MRPPlant,
+		inoutSDC.PlannedOrder.Component.Plant,
+		accepter,
+	)
+}
